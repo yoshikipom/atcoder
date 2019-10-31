571d31aa4a71b987db7b47b8677dcf345063c9a7
Yoshiki Shino <yoshiki.shino.tech@gmail.com>
2019-10-31 21:22:22 +0900

    [Update]

--- abc/143/d.py
+++ abc/143/d.py
@@ -1,3 +1,6 @@
+import bisect
+
+
 def bs(L, left, right, bc):
     while left != right:
         mid = (left + right)//2
@@ -14,22 +17,15 @@ def solve():
     N = int(input())
     L = list(map(int, input().split()))
     L.sort()
-    # print(L)
     count = 0
     for i in range(1, N-1):
         for j in range(i + 1, N):
-            left = 0
-            right = i-1
-            div = bs(L, left, right, L[j] - L[i])
+            div = bisect.bisect_right(L[0:i], L[j] - L[i])
             if (div != -1):
                 count += i - div
-                # print('counted')
-                # print(i-div)
 
     print(count)
 
 
 if __name__ == "__main__":
     solve()
-    # L = [1, 2, 3, 4, 5, 6]
-    # print(bs(L, 0, 3, 1))
