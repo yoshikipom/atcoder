210a83097e02c212f0ef66755b7b47ec041b0ff9
Yoshiki Shino <yoshiki.shino.tech@gmail.com>
2022-10-29 22:54:09 +0900

    fix e

--- abc/275/e.py
+++ abc/275/e.py
@@ -1,9 +1,11 @@
 MOD = 998244353
 
+
 def main():
     n, m, k = list(map(int, input().split()))
 
-    dp = [[0 for j in range(n+1)] for i in range(k+1)]  # [ターン][どこにいるか] = case数
+    dp = [[0 for j in range(n+1)] for i in range(k+1)]  # [ターン][どこにいるか] = 確率
+    m_inv = pow(m, MOD-2, MOD)
 
     dp[0][0] = 1
     for i in range(k):
@@ -11,34 +13,16 @@ def main():
             for a in range(1, m+1):
                 # print(i,j,a)
                 if j + a > n:
-                    dp[i+1][2*n - j-a] += dp[i][j]
+                    dp[i+1][2*n - j-a] += dp[i][j] * m_inv % MOD
                 else:
-                    dp[i+1][j+a] += dp[i][j]
-        # dp[i+1][n] += dp[i][n]
-    
-    print('debug')
-    for row in dp:
-        print(*row)
+                    dp[i+1][j+a] += dp[i][j] * m_inv % MOD
+        dp[i+1][n] += dp[i][n]
 
-    numerator = dp[-1][-1] % MOD
-    denominator = sum(dp[-1])
-    # print(numerator, denominator)
-    result = (numerator * pow(denominator, MOD-2, MOD)) % MOD
-    print(result)
+    # print('debug')
+    # for row in dp:
+    #     print(*row)
 
-    numerator = 0
-    denominator = 0
-    result = 0
-    for i in range(1, k+1):
-        numerator += dp[i][-1] % MOD
-        denominator += sum(dp[i]) % MOD
-        result += numerator * pow(denominator, MOD-2, MOD) % MOD
-        print(i)
-        print(dp[i][-1], sum(dp[i]))
-        print(result)
-    # print(numerator, denominator)
-    print(result)
-
+    print(dp[-1][-1] % MOD)
 
 
 if __name__ == '__main__':
